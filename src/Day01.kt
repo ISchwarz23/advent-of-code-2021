3299068965bb38d3ebbaf555ed74f5b9a85bdd63
ischwarz <ingo.schwarz23@gmail.com>
2021-12-01 06:31:36 +0100

    Provide solution for day 1 - part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -13,12 +13,27 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val intInput: List<Int> = input.map { it.toInt() }
+        fun getFrameSum(index: Int): Int {
+            return intInput[index - 1] + intInput[index] + intInput[index + 1]
+        }
+
+        var increaseCounter = 0
+        var previousValue = Integer.MAX_VALUE
+
+        for(i in 1..intInput.size-2) {
+            val frameSum = getFrameSum(i)
+            if(frameSum > previousValue)
+                increaseCounter++
+            previousValue = frameSum
+        }
+        return increaseCounter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
