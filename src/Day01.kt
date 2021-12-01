5ccc2bd7adc452f56c50f2137c605f113d18a2b4
ischwarz <ingo.schwarz23@gmail.com>
2021-12-01 06:13:35 +0100

    Provide solution for day 1 - part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,15 @@
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increaseCounter = 0
+        var previousValue = Integer.MAX_VALUE
+
+        input.map { it.toInt() }.forEach {
+            if(it > previousValue)
+                increaseCounter++
+            previousValue = it
+        }
+        return increaseCounter
     }
 
     fun part2(input: List<String>): Int {
@@ -10,7 +18,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
