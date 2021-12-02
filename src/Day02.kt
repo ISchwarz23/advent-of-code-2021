7fe837cb3c05a217f4b9eb7e6dad6341d241a309
ischwarz <ingo.schwarz23@gmail.com>
2021-12-02 08:11:32 +0100

    Implement solution for day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,16 +1,28 @@
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var position = 0
+        var depth = 0
+
+        input.map { it.split(" ") }
+            .map { it[0] to it[1].toInt() }
+            .forEach {
+                when(it.first) {
+                    "forward" -> position += it.second
+                    "down" -> depth += it.second
+                    "up" -> depth -= it.second
+                }
+            }
+        return position * depth
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 150)
     check(part2(testInput) == 0)
 
     val input = readInput("Day02")
