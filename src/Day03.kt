0a6e6e656acd9516f10991f58dac8de3d4e1cc92
ischwarz <ingo.schwarz23@gmail.com>
2021-12-03 08:13:04 +0100

    Implement solution for day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,7 +1,29 @@
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        if(input.isEmpty()) return 0
+
+        val counters = IntArray(input[0].length)
+        input.forEach {
+            it.forEachIndexed { index, c ->
+                if(c == '1') counters[index] += 1
+            }
+        }
+
+        val majority = input.size / 2
+        var gammaRate = ""
+        var epsilonRate = ""
+        counters.forEach {
+            if(it > majority) {
+                gammaRate += "1"
+                epsilonRate += "0"
+            } else {
+                gammaRate += "0"
+                epsilonRate += "1"
+            }
+        }
+        return Integer.parseInt(gammaRate, 2) *
+                Integer.parseInt(epsilonRate, 2)
     }
 
     fun part2(input: List<String>): Int {
@@ -10,7 +32,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 0)
+    check(part1(testInput) == 198)
     check(part2(testInput) == 0)
 
     val input = readInput("Day03")
