906cc72dec12df57e28b4ef6242c0882b077b2a2
ischwarz <ingo.schwarz23@gmail.com>
2021-12-06 06:32:03 +0100

    Implement solution for day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,19 +1,26 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return 0
+    // 3,4,3,1,2
+    fun part1(input: List<Int>): Int {
+        var fishAges: MutableList<Int> = input.toMutableList()
+        for(i in 0 until 80) {
+            val fishToAdd = fishAges.count { it == 0 }
+            fishAges = fishAges.map { if(it > 0) it - 1 else 6 }.toMutableList()
+            for(n in 0 until fishToAdd) fishAges.add( 8 )
+        }
+        return fishAges.size
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<Int>): Int {
         return 0
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06_test")
-    check(part1(testInput) == 0)
+    val testInput = readOneLineInputAsInts("Day06_test")
+    check(part1(testInput) == 5934)
     check(part2(testInput) == 0)
 
-    val input = readInput("Day06")
+    val input = readOneLineInputAsInts("Day06")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,13 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Reads one line and map to Ints.
+ */
+fun readOneLineInputAsInts(name: String, delimiter: String = ","): List<Int> {
+    return readInput(name)[0].split(delimiter).map { it.toInt() }
+}
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
