c480d4ea9cb7c5768cbe562191fa7d376a3e2bf1
ischwarz <ingo.schwarz23@gmail.com>
2021-12-09 20:36:35 +0100

    Prepare day 10

---
+++ src/main/kotlin/com/schwarz/aoc/Day10.kt
@@ -0,0 +1,13 @@
+package com.schwarz.aoc
+
+object Day10 {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+}
