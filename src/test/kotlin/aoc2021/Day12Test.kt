d47d7b3eb67099f70e47f14f7ffa1029fe73de50
ischwarz <ingo.schwarz23@gmail.com>
2021-12-12 08:36:59 +0100

    Implement solution for day 12

--- src/test/kotlin/aoc2021/Day12Test.kt
+++ src/test/kotlin/aoc2021/Day12Test.kt
@@ -11,8 +11,8 @@ import kotlin.test.assertEquals
 )
 internal class Day12Test {
 
-    private val testInput = readInput("Day12_test")
-    private val input = readInput("Day12")
+    private val testInput = readInputAsCaveConnections("Day12_test")
+    private val input = readInputAsCaveConnections("Day12")
 
     @Test
     internal fun testPart1() {
@@ -20,7 +20,7 @@ internal class Day12Test {
         val result = Day12.part1(testInput)
 
         // then
-        assertEquals(0, result)
+        assertEquals(10, result)
 
         // get solution
         println("Result of Day 12 - Part 1: ${Day12.part1(input)}")
@@ -32,10 +32,19 @@ internal class Day12Test {
         val result = Day12.part2(testInput)
 
         // then
-        assertEquals(0, result)
+        assertEquals(36, result)
 
         // get solution
         println("Result of Day 12 - Part 2: ${Day12.part2(input)}")
     }
+
+    private fun readInputAsCaveConnections(name: String): Map<String, List<String>> {
+        return readInput(name).map { it.split("-") }
+            .map { listOf(CaveConnection(it[0], it[1]), CaveConnection(it[1], it[0])) }
+            .flatten()
+            .groupBy(CaveConnection::from, CaveConnection::to)
+    }
+
+    data class CaveConnection(val from: String, val to: String )
 
 }
