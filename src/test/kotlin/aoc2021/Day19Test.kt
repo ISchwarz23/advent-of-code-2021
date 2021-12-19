74e183a4b5a0d2344aef1168e911a5560d39c4d6
Ingo Schwarz <ingo.schwarz23@gmail.com>
2021-12-19 21:07:12 +0100

    Implement solution for day 19

--- src/test/kotlin/aoc2021/Day19Test.kt
+++ src/test/kotlin/aoc2021/Day19Test.kt
@@ -3,16 +3,18 @@ package aoc2021
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.TestMethodOrder
 import utils.readInput
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @TestMethodOrder(
     MethodOrderer.Alphanumeric::class
 )
+@Ignore("Ignore as proecessing is pretty slow")
 internal class Day19Test {
 
-    private val testInput = readInput("Day19_test")
-    private val input = readInput("Day19")
+    private val testInput = readInputAsScannerResults("Day19_test")
+    private val input = readInputAsScannerResults("Day19")
 
     @Test
     internal fun testPart1() {
@@ -20,7 +22,7 @@ internal class Day19Test {
         val result = Day19.part1(testInput)
 
         // then
-        assertEquals(0, result)
+        assertEquals(79, result)
 
         // get solution
         println("Result of Day 19 - Part 1: ${Day19.part1(input)}")
@@ -32,10 +34,36 @@ internal class Day19Test {
         val result = Day19.part2(testInput)
 
         // then
-        assertEquals(0, result)
+        assertEquals(3621, result)
 
         // get solution
         println("Result of Day 19 - Part 2: ${Day19.part2(input)}")
+    }
+
+    private fun readInputAsScannerResults(name: String): List<ScannerResult> {
+        val lines = readInput(name)
+        val scannerResults = mutableListOf<ScannerResult>()
+        var currentScannerId = 0
+        var currentScannerBeacons = mutableListOf<Position3D>()
+        for (line in lines) {
+            if(line.isEmpty()) {
+                scannerResults += ScannerResult(currentScannerId, currentScannerBeacons)
+            } else if(line.startsWith("---")) {
+                val fromIndex = "--- scanner ".length
+                val toIndex = line.indexOf(" ---")
+                currentScannerId = line.substring(fromIndex, toIndex).toInt()
+                currentScannerBeacons = mutableListOf()
+            } else {
+                currentScannerBeacons += line.toPosition3D()
+            }
+        }
+        scannerResults += ScannerResult(currentScannerId, currentScannerBeacons)
+        return scannerResults
+    }
+
+    private fun String.toPosition3D(): Position3D {
+        val coords = split(",").map { it.toInt() }
+        return Position3D(coords[0], coords[1], coords[2])
     }
 
 }
