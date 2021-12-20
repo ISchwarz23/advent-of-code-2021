37c2d78ad4483475b5e8f97774f7391c6f1b1bfe
Ingo Schwarz <ingo.schwarz23@gmail.com>
2021-12-20 11:17:05 +0100

    Prepare day 21

---
+++ src/test/kotlin/aoc2021/Day21Test.kt
@@ -0,0 +1,41 @@
+package aoc2021
+
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.TestMethodOrder
+import utils.readInput
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@TestMethodOrder(
+    MethodOrderer.Alphanumeric::class
+)
+internal class Day21Test {
+
+    private val testInput = readInput("Day21_test")
+    private val input = readInput("Day21")
+
+    @Test
+    internal fun testPart1() {
+        // when
+        val result = Day21.part1(testInput)
+
+        // then
+        assertEquals(0, result)
+
+        // get solution
+        println("Result of Day 21 - Part 1: ${Day21.part1(input)}")
+    }
+
+    @Test
+    internal fun testPart2() {
+        // when
+        val result = Day21.part2(testInput)
+
+        // then
+        assertEquals(0, result)
+
+        // get solution
+        println("Result of Day 21 - Part 2: ${Day21.part2(input)}")
+    }
+
+}
