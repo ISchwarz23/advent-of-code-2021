2577fd37d8a6334e895a0ae9bda7ad8ae0f391c8
Ingo Schwarz <ingo.schwarz23@gmail.com>
2021-12-21 12:27:44 +0100

    Implement solution for day 21

--- src/test/kotlin/aoc2021/Day21Test.kt
+++ src/test/kotlin/aoc2021/Day21Test.kt
@@ -11,31 +11,36 @@ import kotlin.test.assertEquals
 )
 internal class Day21Test {
 
-    private val testInput = readInput("Day21_test")
-    private val input = readInput("Day21")
+    private val testInput = readInputAsPlayerStartPositions("Day21_test")
+    private val input = readInputAsPlayerStartPositions("Day21")
 
     @Test
     internal fun testPart1() {
         // when
-        val result = Day21.part1(testInput)
+        val result = Day21.part1(testInput.first, testInput.second)
 
         // then
-        assertEquals(0, result)
+        assertEquals(739785, result)
 
         // get solution
-        println("Result of Day 21 - Part 1: ${Day21.part1(input)}")
+        println("Result of Day 21 - Part 1: ${Day21.part1(input.first, input.second)}")
     }
 
     @Test
     internal fun testPart2() {
         // when
-        val result = Day21.part2(testInput)
+        val result = Day21.part2(testInput.first, testInput.second)
 
         // then
-        assertEquals(0, result)
+        assertEquals(444356092776315, result)
 
         // get solution
-        println("Result of Day 21 - Part 2: ${Day21.part2(input)}")
+        println("Result of Day 21 - Part 2: ${Day21.part2(input.first, input.second)}")
+    }
+
+    private fun readInputAsPlayerStartPositions(name: String): Pair<Int, Int> {
+        val startPositions = readInput(name).map { it.substring(28) }.map { it.toInt() }
+        return Pair(startPositions[0], startPositions[1])
     }
 
 }
